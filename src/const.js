847c23cef7a5a4b58d7194a697a913bcb4272970
thirdbyron <rgnatovsky@gmail.com>
2022-08-11 13:34:21 +0300

    Добавляет временные данные, создает генераторы случайных значений и выводит данные из модели в шаблон Movie Card

--- src/const.js
+++ src/const.js
@@ -1,1 +1,5 @@
-export const INITIAL_MOVIE_CARDS_QUANTITY = 5;
+export const MINUTES_IN_ONE_HOUR = 60;
+
+export const MAX_SYMBOLS_IN_MOVIE_CARD = 140;
+
+export const CONTINOUS_SYMBOL = '...';
--- src/mock/generate-movie-fish.js
+++ src/mock/generate-movie-fish.js
@@ -1,35 +1,64 @@
-export const generateMovieFish = () => ({
-  'id': '0',
-  'comments': [
-    'test comment 1', 'test comment 2'
-  ],
-  'film_info': {
-    'title': 'A Little Pony Without The Carpet',
-    'alternative_title': 'Laziness Who Sold Themselves',
-    'total_rating': 5.3,
-    'poster': 'images/posters/blue-blazes.jpg',
-    'age_rating': 0,
-    'director': 'Tom Ford',
-    'writers': [
-      'Takeshi Kitano'
-    ],
-    'actors': [
-      'Morgan Freeman'
-    ],
-    'release': {
-      'date': '2019-05-11T00:00:00.000Z',
-      'release_country': 'Finland'
-    },
-    'runtime': 77,
-    'genre': [
-      'Comedy'
-    ],
-    'description': 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
-  },
-  'user_details': {
-    'watchlist': false,
-    'already_watched': true,
-    'watching_date': '2019-04-12T16:12:32.554Z',
-    'favorite': false
+import { getRandomInteger } from '../utils.js';
+import { MOVIE_TITLES_DICTIONARY, MOVIE_DESCRIPTIONS } from '../temporary_data.js';
+
+const generateRandomMovieTitle = (dictionary) => {
+  const titles = Object.keys(dictionary);
+
+  return titles[getRandomInteger(0, titles.length - 1)];
+};
+
+const generateRandomMovieDescription = (descriptions) => {
+
+  const descriptionLength = getRandomInteger(1, descriptions.length);
+
+  let randomDescription = '';
+
+  for (let i = 0; i < descriptionLength; i++) {
+    randomDescription += descriptions[getRandomInteger(0, descriptionLength - 1)];
   }
-});
+
+  return randomDescription;
+};
+
+export const generateMovieFish = () => {
+
+  const randomTitle = generateRandomMovieTitle(MOVIE_TITLES_DICTIONARY);
+  const posterLink = MOVIE_TITLES_DICTIONARY[randomTitle];
+  const randomDescription = generateRandomMovieDescription(MOVIE_DESCRIPTIONS);
+
+  return {
+    'id': '0',
+    'comments': [
+      'test comment 1', 'test comment 2'
+    ],
+    'film_info': {
+      'title': randomTitle,
+      'alternative_title': 'Laziness Who Sold Themselves',
+      'total_rating': 5.3,
+      'poster': posterLink,
+      'age_rating': 0,
+      'director': 'Tom Ford',
+      'writers': [
+        'Takeshi Kitano'
+      ],
+      'actors': [
+        'Morgan Freeman'
+      ],
+      'release': {
+        'date': '2019-05-11T00:00:00.000Z',
+        'release_country': 'Finland'
+      },
+      'runtime': 77,
+      'genre': [
+        'Comedy'
+      ],
+      'description': randomDescription
+    },
+    'user_details': {
+      'watchlist': false,
+      'already_watched': true,
+      'watching_date': '2019-04-12T16:12:32.554Z',
+      'favorite': false
+    }
+  };
+};
