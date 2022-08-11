a80c3eb9e07a67b91668549bf0132e38ddf65be8
thirdbyron <rgnatovsky@gmail.com>
2022-08-11 10:34:59 +0300

    Исправляет ошибку в названии класса, перемещает зависимости из продакшн в режим разработчика

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,7 +1,7 @@
 import { render } from '../render.js';
 import ContentView from '../view/content/content-view.js';
 import MoviesListWrapperView from '../view/content/movies-list-wrapper-view.js';
-import moviesListView from '../view/content/movies-list-view.js';
+import MoviesListView from '../view/content/movies-list-view.js';
 import MovieCardView from '../view/content/movie-card-view.js';
 import ShowMoreButtonView from '../view/content/show-more-button-view.js';
 
@@ -9,7 +9,7 @@ import ShowMoreButtonView from '../view/content/show-more-button-view.js';
 export default class ContentPresenter {
   contentComponent = new ContentView;
   moviesListWrapperComponent = new MoviesListWrapperView;
-  moviesListComponent = new moviesListView;
+  moviesListComponent = new MoviesListView;
   showMoreButtonComponent = new ShowMoreButtonView;
 
   init(mainContainer, moviesModel) {
