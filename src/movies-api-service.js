3f25bfa891dc39d5f420c6a2ae3fa07fe4f18b48
thirdbyron <rgnatovsky@gmail.com>
2022-08-26 18:46:03 +0300

    Рефакторит patch обновление

---
+++ src/movies-api-service.js
@@ -0,0 +1,23 @@
+import { METHOD } from './const.js';
+import ApiService from './framework/api-service.js';
+
+export default class MoviesApiService extends ApiService {
+
+  get movies() {
+    return this._load({url: 'movies'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateMovie = async (movie) => {
+    const response = await this._load({
+      url: `movies/${movie.id}`,
+      method: METHOD.PUT,
+      body: JSON.stringify(movie),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  };
+}
